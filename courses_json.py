5dc9025b22478b0bb67287eeaad05cfe2fad8dc5
Birodiallo721 <birodiallo721@gmail.com>
2025-04-10 12:10:33 +0000

    json add

--- courses_json.py
+++ courses_json.py
@@ -1,5 +1,7 @@
 import sys
+import json
 choix = ""
+chemin = "D:\ProjetsBackEnd\ListCoursesWithJson\donnees.json"
 list_courses = []
     
 while not(choix.isdigit()) or not(1 <= int(choix) <=5):
@@ -16,21 +18,39 @@ while not(choix.isdigit()) or not(1 <= int(choix) <=5):
         
     if choix == "1":
         courses =  input("Entrez le nom d'un element a ajouter a la liste de courses : ")
+        with open(chemin, mode = "r") as fichier:
+         list_courses = json.load(fichier)
+       
         list_courses.append(courses)
+       
+        with open(chemin, mode = "w") as fichier:
+          json.dump(list_courses,fichier,indent = 4)
+
         print(f"L'element {courses} a bien ete ajoute a la liste.")
         choix = ""
 
     elif choix == "2":
         retirer = input("Entrez le nom d'un element a retirer de la liste de courses : ") 
         if list_courses.count(retirer):
+
+            with open(chemin, mode = "r") as fichier:
+             list_courses = json.load(fichier)
+       
             list_courses.remove(retirer)
+       
+            with open(chemin, mode = "w") as fichier:
+                json.dump(list_courses,fichier,indent = 4)
+
             print(f"L'element {retirer} a bien ete suprimer de la liste.")
         else:
                 print(f"L'element {retirer} n'est pas dans la liste.")   
         choix = ""        
 
     elif choix == "3":
-                
+
+        with open(chemin, mode = "r") as fichier:
+            list_courses = json.load(fichier)
+
         if list_courses:        
             print("Voici le contenu de votre liste : ") 
             for i, course in enumerate(list_courses,1):
@@ -40,7 +60,16 @@ while not(choix.isdigit()) or not(1 <= int(choix) <=5):
         choix = ""    
 
     elif choix == "4":
+
+        with open(chemin, mode = "r") as fichier :
+            list_courses = json.load(fichier)
+
         list_courses.clear(),
+
+        fichier =  open(chemin, mode = "w")
+        json.dump(list_courses,fichier,indent = 4)
+        fichier.close()
+
         print("La liste a ete videe de son contenu") 
         choix = ""    
                 
